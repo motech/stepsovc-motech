846f8880d9053cec7f22cfedec7da56ac76b1996
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-05-17 16:48:30 +0530

    Geetha, Sathish |#180|  Added missing vo's

---
+++ stepsovc-commcare-api/src/main/java/org/wv/stepsovc/commcare/vo/BeneficiaryInformation.java
@@ -0,0 +1,138 @@
+package org.wv.stepsovc.commcare.vo;
+
+
+public class BeneficiaryInformation {
+
+    private String beneficiaryId;
+
+    private String beneficiaryName;
+
+    private String beneficiaryDob;
+
+    private String beneficiarySex;
+
+    private String beneficiaryCode;
+
+    private String beneficiaryTitle;
+
+    private String receivingOrganization;
+
+    private String careGiverId;
+
+    private String careGiverCode;
+
+    private String careGiverName;
+
+    private String ownerId;
+
+
+    /* date of created of beneficiary case in commcare */
+    private String dateModified;
+
+    /* case type is Beneficiary */
+    private String caseType;
+
+    public String getBeneficiaryId() {
+        return beneficiaryId;
+    }
+
+    public void setBeneficiaryId(String beneficiaryId) {
+        this.beneficiaryId = beneficiaryId;
+    }
+
+    public String getBeneficiaryName() {
+        return beneficiaryName;
+    }
+
+    public void setBeneficiaryName(String beneficiaryName) {
+        this.beneficiaryName = beneficiaryName;
+    }
+
+    public String getBeneficiaryDob() {
+        return beneficiaryDob;
+    }
+
+    public void setBeneficiaryDob(String beneficiaryDob) {
+        this.beneficiaryDob = beneficiaryDob;
+    }
+
+    public String getBeneficiarySex() {
+        return beneficiarySex;
+    }
+
+    public void setBeneficiarySex(String beneficiarySex) {
+        this.beneficiarySex = beneficiarySex;
+    }
+
+    public String getBeneficiaryCode() {
+        return beneficiaryCode;
+    }
+
+    public void setBeneficiaryCode(String beneficiaryCode) {
+        this.beneficiaryCode = beneficiaryCode;
+    }
+
+    public String getBeneficiaryTitle() {
+        return beneficiaryTitle;
+    }
+
+    public void setBeneficiaryTitle(String beneficiaryTitle) {
+        this.beneficiaryTitle = beneficiaryTitle;
+    }
+
+    public String getReceivingOrganization() {
+        return receivingOrganization;
+    }
+
+    public void setReceivingOrganization(String receivingOrganization) {
+        this.receivingOrganization = receivingOrganization;
+    }
+
+    public String getCareGiverId() {
+        return careGiverId;
+    }
+
+    public void setCareGiverId(String careGiverId) {
+        this.careGiverId = careGiverId;
+    }
+
+    public String getCareGiverCode() {
+        return careGiverCode;
+    }
+
+    public void setCareGiverCode(String careGiverCode) {
+        this.careGiverCode = careGiverCode;
+    }
+
+    public String getDateModified() {
+        return dateModified;
+    }
+
+    public void setDateModified(String dateModified) {
+        this.dateModified = dateModified;
+    }
+
+    public String getCaseType() {
+        return caseType;
+    }
+
+    public void setCaseType(String caseType) {
+        this.caseType = caseType;
+    }
+
+    public String getCareGiverName() {
+        return careGiverName;
+    }
+
+    public void setCareGiverName(String careGiverName) {
+        this.careGiverName = careGiverName;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+}
---
+++ stepsovc-commcare-api/src/main/java/org/wv/stepsovc/commcare/vo/CareGiverInformation.java
@@ -0,0 +1,61 @@
+package org.wv.stepsovc.commcare.vo;
+
+
+public class CareGiverInformation {
+
+    /* primary key column cg_id - from DMIS (id)*/
+    private String id;
+
+    /* user name to login - from DMIS*/
+    private String code;
+
+    /* password to login - generated */
+    private String password;
+
+    /* name of the care giver - from DMIS */
+    private String name;
+
+    /* phone number of care giver - from DMIS */
+    private String phoneNumber;
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
