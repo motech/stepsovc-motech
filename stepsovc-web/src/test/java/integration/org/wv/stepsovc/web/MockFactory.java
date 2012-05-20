fae1af8278bff8c135d11720d4cd3b851bf7b0f2
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-05-21 01:49:16 +0530

    Balaji/Alex | 159  SMS Alerts-test fix

--- stepsovc-web/src/test/java/integration/org/wv/stepsovc/web/MockFactory.java
+++ stepsovc-web/src/test/java/integration/org/wv/stepsovc/web/MockFactory.java
@@ -1,10 +1,17 @@
 package org.wv.stepsovc.web;
 
+import org.motechproject.sms.api.service.SmsAuditService;
+import org.motechproject.sms.api.service.SmsService;
+
 import static org.mockito.Mockito.mock;
 
 public class MockFactory {
 
-    public static <T> T create(Class<T> c) {
-        return mock(c);
+    public static SmsAuditService createAuditService() {
+        return mock(SmsAuditService.class);
+    }
+
+    public static SmsService createSmsService() {
+        return mock(SmsService.class);
     }
 }
