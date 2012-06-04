08849bb7d893576e54271e95b3e734d69fb1fd04
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-06-04 16:37:23 +0530

    Balaji/Dhivya : Refactored cgServiceTest to CaregiverServiceTest

--- stepsovc-core/src/test/unit/org/wv/stepsovc/core/services/CaregiverServiceTest.java
+++ stepsovc-core/src/test/unit/org/wv/stepsovc/core/services/CaregiverServiceTest.java
@@ -12,7 +12,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-public class cgServiceTest {
+public class CaregiverServiceTest {
 
     CaregiverService caregiverService;
 
