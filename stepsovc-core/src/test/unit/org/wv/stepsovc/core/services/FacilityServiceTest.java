e9e8ca9090e5a10b75b29583381e7136d1ce4638
Geetha Balasundaram <geetha.balasundaram@gmail.com>
2012-06-12 17:22:42 +0530

    Geetha : #165 Completed alert caregivers when service unavail form is uploaded by healthworker.

--- stepsovc-core/src/test/unit/org/wv/stepsovc/core/services/FacilityServiceTest.java
+++ stepsovc-core/src/test/unit/org/wv/stepsovc/core/services/FacilityServiceTest.java
@@ -3,12 +3,18 @@ package org.wv.stepsovc.core.services;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.motechproject.cmslite.api.model.ContentNotFoundException;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.wv.stepsovc.core.domain.Facility;
+import org.wv.stepsovc.core.domain.Referral;
 import org.wv.stepsovc.core.domain.ServiceUnavailability;
+import org.wv.stepsovc.core.fixtures.StepsovcCaseFixture;
 import org.wv.stepsovc.core.mapper.FacilityMapper;
 import org.wv.stepsovc.core.repository.AllFacilities;
+import org.wv.stepsovc.core.request.StepsovcCase;
+import org.wv.stepsovc.core.utils.DateUtils;
 
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,6 +32,10 @@ public class FacilityServiceTest {
     private String serviceDate = "2012-01-02";
     @Mock
     private FacilityMapper mockFacilityMapper;
+    @Mock
+    private StepsovcAlertService mockAlertService;
+    @Mock
+    private ReferralService mockReferralService;
 
 
     @Before
@@ -34,6 +44,8 @@ public class FacilityServiceTest {
         facilityService = new FacilityService();
         ReflectionTestUtils.setField(facilityService, "allFacilities", mockAllFacilities);
         ReflectionTestUtils.setField(facilityService, "facilityMapper", mockFacilityMapper);
+        ReflectionTestUtils.setField(facilityService, "stepsovcAlertService", mockAlertService);
+        ReflectionTestUtils.setField(facilityService, "referralService", mockReferralService);
     }
 
     @Test
@@ -96,6 +108,22 @@ public class FacilityServiceTest {
         when(mockFacilityMapper.map(existingFacility, facility)).thenReturn(facility);
         facilityService.addFacility(facility);
         verify(mockAllFacilities).update(facility);
+    }
+
+    @Test
+    public void shouldInvokeAlertServiceToSendInstantMsgToCareGiverWhenFacilityIsUnavailable() throws ParseException, ContentNotFoundException {
+        String facilityCode = "testCode";
+        String unavailableDate = "2012-12-12";
+        String nextAvailableDate = "2012-12-13";
+        StepsovcCase facilityCase = StepsovcCaseFixture.createNewCaseWithFacilityCode(facilityCode, unavailableDate, unavailableDate);
+
+        List<Referral> updatedReferrals = Arrays.asList(new Referral());
+        doReturn(updatedReferrals).when(mockReferralService).updateReferralsServiceDate(facilityCode, facilityCase.getService_unavailable_from(),
+                facilityCase.getService_unavailable_to(), DateUtils.nextDateStr(facilityCase.getService_unavailable_to()));
+        facilityService.makeFacilityUnavailable(facilityCase);
+
+        verify(mockAlertService).sendInstantServiceUnavailabilityMsgToCareGivers(updatedReferrals, facilityCode, unavailableDate, unavailableDate, nextAvailableDate);
+
     }
 
 
