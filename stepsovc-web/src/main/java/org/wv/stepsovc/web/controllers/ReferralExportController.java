b89856c383647f9a6d0ad2140c4a6514cf8f6e20
Sathishkumar Thangavel <tsathish@thoughtworks.com>
2012-06-22 15:09:11 +0530

    Sathish,Geetha | Added authorization to export controller

--- stepsovc-web/src/main/java/org/wv/stepsovc/web/controllers/ReferralExportController.java
+++ stepsovc-web/src/main/java/org/wv/stepsovc/web/controllers/ReferralExportController.java
@@ -2,10 +2,12 @@ package org.wv.stepsovc.web.controllers;
 
 import org.motechproject.export.service.ExportService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -18,12 +20,18 @@ public class ReferralExportController {
     public static final String TEXT_CSV = "text/csv";
     @Autowired
     private ExportService exportService;
+    @Value("#{stepovcProperties['export.key']}")
+    private String key;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{groupName}/{reportName}.csv")
     public void exportReferralsAsCSV(@PathVariable("groupName") String groupName, @PathVariable("reportName") String reportName,
-                                     HttpServletResponse response) throws IOException {
+                                     @RequestParam("key") String requestKey, HttpServletResponse response) throws IOException {
         initializeCSVResponse(response, reportName + ".csv");
-        exportService.exportAsCSV(groupName, response.getWriter());
+
+        if (key.equals(requestKey))
+            exportService.exportAsCSV(groupName, response.getWriter());
+        else
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 
     private void initializeCSVResponse(HttpServletResponse response, String fileName) {
