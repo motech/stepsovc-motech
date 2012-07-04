2b64f202da964f912e82be13558a4a1d8f91deac
Balaji G <balajig@Balajis-MacBook-Pro.local>
2012-07-04 15:37:10 +0530

    Balaji/Dhivhya : Added  Index.html,Log4j  auto load.

--- stepsovc-web/src/main/java/org/wv/stepsovc/web/controllers/TestController.java
+++ stepsovc-web/src/main/java/org/wv/stepsovc/web/controllers/TestController.java
@@ -1,6 +1,7 @@
 package org.wv.stepsovc.web.controllers;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,9 +11,15 @@ import java.util.Date;
 @Controller
 public class TestController {
 
+
+    private Logger logger = Logger.getLogger(this.getClass());
+
     @RequestMapping(value = "/test")
     @ResponseBody
     public String loadHelloWorld() {
+        logger.info("***********Test PAGE- Log  configured in Info  Mode*************** ");
+        logger.debug("***********Test PAGE- Log  configured in DEBUG  Mode*************** ");
+        logger.error("***********Test PAGE- Log  configured in Error  Mode*************** ");
         return " Testing - StepOVC Running @" + new Date();
     }
 }
